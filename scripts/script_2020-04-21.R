c32a7ccf699ed146ed8a0ce900815b126f64c112
Antoine Bichat <a.bichat@yahoo.fr>
2020-04-21 12:19:30 +0200

    data update

--- scripts/script_2020-04-21.R
+++ scripts/script_2020-04-21.R
@@ -22,12 +22,10 @@ articles_violated <-
                                                str_replace, "Art\\.? ?", "Art. ")) %>%
   select(-price, -article_violated, -article_violated_splitted)
 
-limit <- 2
-limit_chr <- "two"
 counts <-
   articles_violated %>% 
   count(major_article_violated_splitted, sort = TRUE) %>% 
-  group_by(happedalot = n > limit) %>% 
+  group_by(happedone = n == 1) %>% 
   summarise(unique_composition = n(), total_by_composition = sum(n), 
             article = c(major_article_violated_splitted)) %>% 
   nest(article = article) %>%
@@ -39,36 +37,41 @@ counts <-
 #### Plot ####
 
 family <- "Roboto Condensed"
+txtcolor <- "#C7AB47"
 
 p <- 
   ggplot(articles_violated) +
   aes(x = major_article_violated_splitted) +
-  geom_bar(fill = "grey70") +
+  geom_text(label = glue("Overall, {nrow(articles_violated)} fines for violating specific
+                         GDPR articles have been issued."), 
+            x = 12, y = 25, size = 5, color = txtcolor, family = family, hjust = 0) +
+  geom_bar(fill = "grey70", color = "black") +
   scale_y_continuous(breaks = c(10, 30, 50, 70)) +
-  scale_x_upset(order_by = "freq", n_intersections = counts$unique_composition[2]) +
+  scale_x_upset(order_by = "freq", n_intersections = counts$unique_composition[1]) +
   labs(x = NULL, y = NULL, 
-       title = "Count of GDPR articles violations compositions",
-       subtitle = glue("Compositions that happened {limit_chr} times or less are not displayed \\
-                       ({counts$total_by_composition[1]} fines and \\
-                       {length(setdiff(counts$article[[1]], counts$article[[2]]))} \\
-                       other articles)."),
+       title = "GDPR fines by articles violations compositions",
+       subtitle = glue("Compositions that happened only one time are not \\
+                        displayed ({counts$total_by_composition[2]} fines and \\
+                        {length(setdiff(counts$article[[2]], counts$article[[1]]))} \\
+                        other articles)."),
        caption = "Source: Privacy Affairs\n@_abichat for #TidyTuesday") +
   theme_minimal() +
-  theme(title = element_text(color = "#C7AB47", size = 20, family = family), 
+  theme(title = element_text(color = txtcolor, size = 20, family = family), 
         plot.subtitle = element_text(size = 13),
         plot.caption = element_text(size = 11),
         plot.title.position = "plot",
-        axis.text.y = element_text(hjust = 0, color = "#C7AB47", size = 10, family = family),
+        axis.text.y = element_text(hjust = 0, color = txtcolor, size = 10, family = family),
         panel.grid = element_line(color = "#C6AE62"),
         panel.grid.major.x = element_blank(),
         panel.grid.minor.y = element_line(size = 0.2),
         panel.background = element_rect(fill = "grey20", color = "grey20"),
-        plot.background = element_rect(fill = "grey10")) +
+        plot.background = element_rect(fill = "grey10"), 
+        plot.margin = margin(10, 10, 10, 10)) +
   theme_combmatrix(combmatrix.panel.point.color.fill = "black", 
                    combmatrix.panel.point.color.empty = "grey70",
                    combmatrix.panel.striped_background.color.one = "grey90",
                    combmatrix.panel.striped_background.color.two = "#C6BA95", 
-                   combmatrix.label.height = unit(10, "cm"))
+                   combmatrix.label.height = unit(9, "cm"))
 
 ggsave("plots/plot_2020-04-21.png", p, width = 29, height = 21, units = "cm", dpi = "retina")
 
